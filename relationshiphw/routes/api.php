cc04295cd2d57e4f6df857f9ecd202e051e8e875
dyna2002chhan <dyna.chhan@student.passerellesnumeriques.org>
2021-10-17 22:55:59 +0700

    last commit lararelationship dyna

--- relationshiphw/routes/api.php
+++ relationshiphw/routes/api.php
@@ -17,3 +17,35 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+//Profile route
+Route::get('/profiles', [ProfileController::class, 'index']);
+Route::get('/profiles/{id}', [ProfileController::class, 'show']);
+Route::post('/profiles', [ProfileController::class, 'store']);
+Route::put('/profiles/{id}', [ProfileController::class, 'update']);
+Route::delete('/profiles/{id}', [ProfileController::class, 'destroy']);
+//Post route
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{id}', [PostController::class, 'show']);
+Route::put('/posts/{id}', [PostController::class, 'update']);
+Route::post('/posts', [PostController::class, 'store']);
+Route::delete('/posts/{id}', [PostController::class, 'destroy']);
+//Comment route
+Route::get('/comments/{id}', [CommentController::class, 'show']);
+Route::put('/comments/{id}', [CommentController::class, 'update']);
+Route::get('/comments', [CommentController::class, 'index']);
+Route::post('/comments', [CommentController::class, 'store']);
+Route::delete('/comments/{id}', [CommentController::class, 'destroy']);
+
+//User route
+Route::get('/users/{id}', [UserController::class, 'show']);
+Route::put('/users/{id}', [UserController::class, 'update']);
+Route::get('/users', [UserController::class, 'index']);
+Route::post('/users', [UserController::class, 'store']);
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+//Roles route
+Route::get('/roles/{id}', [RolesController::class, 'show']);
+Route::put('/roles/{id}', [RolesController::class, 'update']);
+Route::get('/roles', [RolesController::class, 'index']);
+Route::post('/roles', [RolesController::class, 'store']);
+Route::delete('/roles/{id}', [RolesController::class, 'destroy']);
